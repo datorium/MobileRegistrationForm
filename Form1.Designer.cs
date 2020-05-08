726aa60f08387f8b78f70e20a07e4c13bf3da4e7
Datorium <hello@datorium.lv>
2020-05-08 16:26:13 +0300

    version initial design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.fieldName = new System.Windows.Forms.TextBox();
+            this.fieldSurname = new System.Windows.Forms.TextBox();
+            this.fieldEmail = new System.Windows.Forms.TextBox();
+            this.fieldPassword = new System.Windows.Forms.TextBox();
+            this.fieldPasswordRepeat = new System.Windows.Forms.TextBox();
+            this.fieldAge = new System.Windows.Forms.NumericUpDown();
+            this.buttonSignUp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fieldAge)).BeginInit();
             this.SuspendLayout();
+            // 
+            // fieldName
+            // 
+            this.fieldName.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fieldName.Location = new System.Drawing.Point(12, 12);
+            this.fieldName.Name = "fieldName";
+            this.fieldName.Size = new System.Drawing.Size(370, 41);
+            this.fieldName.TabIndex = 0;
+            this.fieldName.Text = "fieldName";
+            // 
+            // fieldSurname
+            // 
+            this.fieldSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fieldSurname.Location = new System.Drawing.Point(12, 59);
+            this.fieldSurname.Name = "fieldSurname";
+            this.fieldSurname.Size = new System.Drawing.Size(370, 41);
+            this.fieldSurname.TabIndex = 1;
+            this.fieldSurname.Text = "fieldSurname";
+            // 
+            // fieldEmail
+            // 
+            this.fieldEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fieldEmail.Location = new System.Drawing.Point(12, 106);
+            this.fieldEmail.Name = "fieldEmail";
+            this.fieldEmail.Size = new System.Drawing.Size(370, 41);
+            this.fieldEmail.TabIndex = 2;
+            this.fieldEmail.Text = "fieldEmail";
+            // 
+            // fieldPassword
+            // 
+            this.fieldPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fieldPassword.Location = new System.Drawing.Point(12, 239);
+            this.fieldPassword.Name = "fieldPassword";
+            this.fieldPassword.Size = new System.Drawing.Size(370, 41);
+            this.fieldPassword.TabIndex = 3;
+            this.fieldPassword.Text = "fieldPassword";
+            // 
+            // fieldPasswordRepeat
+            // 
+            this.fieldPasswordRepeat.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fieldPasswordRepeat.Location = new System.Drawing.Point(12, 286);
+            this.fieldPasswordRepeat.Name = "fieldPasswordRepeat";
+            this.fieldPasswordRepeat.Size = new System.Drawing.Size(370, 41);
+            this.fieldPasswordRepeat.TabIndex = 4;
+            this.fieldPasswordRepeat.Text = "fieldPasswordRepeat";
+            // 
+            // fieldAge
+            // 
+            this.fieldAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.fieldAge.Location = new System.Drawing.Point(12, 153);
+            this.fieldAge.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.fieldAge.Minimum = new decimal(new int[] {
+            14,
+            0,
+            0,
+            0});
+            this.fieldAge.Name = "fieldAge";
+            this.fieldAge.Size = new System.Drawing.Size(120, 41);
+            this.fieldAge.TabIndex = 5;
+            this.fieldAge.Value = new decimal(new int[] {
+            14,
+            0,
+            0,
+            0});
+            // 
+            // buttonSignUp
+            // 
+            this.buttonSignUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.buttonSignUp.Location = new System.Drawing.Point(12, 409);
+            this.buttonSignUp.Name = "buttonSignUp";
+            this.buttonSignUp.Size = new System.Drawing.Size(370, 62);
+            this.buttonSignUp.TabIndex = 6;
+            this.buttonSignUp.Text = "Sign Up";
+            this.buttonSignUp.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 401);
+            this.ClientSize = new System.Drawing.Size(394, 531);
+            this.Controls.Add(this.buttonSignUp);
+            this.Controls.Add(this.fieldAge);
+            this.Controls.Add(this.fieldPasswordRepeat);
+            this.Controls.Add(this.fieldPassword);
+            this.Controls.Add(this.fieldEmail);
+            this.Controls.Add(this.fieldSurname);
+            this.Controls.Add(this.fieldName);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fieldAge)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox fieldName;
+        private System.Windows.Forms.TextBox fieldSurname;
+        private System.Windows.Forms.TextBox fieldEmail;
+        private System.Windows.Forms.TextBox fieldPassword;
+        private System.Windows.Forms.TextBox fieldPasswordRepeat;
+        private System.Windows.Forms.NumericUpDown fieldAge;
+        private System.Windows.Forms.Button buttonSignUp;
     }
 }
 
